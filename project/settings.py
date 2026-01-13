88e0751996d5f99c903ec0f4d51cf7b2e04e3422
Your Name <youremail@example.com>
2026-01-13 18:15:45 +0500

    Use environment variables

--- project/settings.py
+++ project/settings.py
@@ -19,11 +19,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
+import os
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t^o6q7_hd$l^btm6cdz9s2m(3%6_)&(j$++(nula90ry90v-!9'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-t^o6q7_hd$l^btm6cdz9s2m(3%6_)&(j$++(nula90ry90v-!9')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
